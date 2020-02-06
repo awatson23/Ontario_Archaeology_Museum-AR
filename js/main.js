839f106df0670124ba48b7f45b7d5436d487cfcd
Jin Chang <chanjinw95@gmail.com>
2020-02-06 09:56:01 -0500

    adding final changes to index.html

--- js/main.js
+++ js/main.js
@@ -53,3 +53,22 @@
       }
       requestAnimationFrame(tick);
     }
+
+    // burgerMenu function
+    
+    (function(){
+      "use strict";	
+      console.log("fired");
+      var button = document.querySelector("#button");
+      var burgerMenu = document.querySelector("#burgerMenu");
+    
+    function hamburgerMenu() {
+      burgerMenu.classList.toggle("slideToggle");
+      button.classList.toggle("expanded");
+    }
+    
+    button.addEventListener("click", hamburgerMenu, false);
+        
+    })();
+    
+    
