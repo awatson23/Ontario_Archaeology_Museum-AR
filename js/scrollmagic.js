48df3b4c588f5fc3ddf341fd9397cd87cae549a7
unknown <awatson23>
2020-02-07 12:57:55 -0500

    re-engineered some of the css on details page

--- js/scrollmagic.js
+++ js/scrollmagic.js
@@ -2,8 +2,24 @@ var controller = new ScrollMagic.Controller();
 
 var scrolldetail1 = new ScrollMagic.Scene({
     triggerElement: '.circ',
-    triggerHook: .8,
-    reverse:false
+    triggerHook: .7,
+    reverse: true
      })
     .setClassToggle('.circ', 'scroll-anim')
+    .addTo(controller);
+
+var scrolldetail2 = new ScrollMagic.Scene({
+    triggerElement: '.potSize p',
+    triggerHook: 1,
+    reverse: true
+        })
+    .setClassToggle('.potSize p', 'scroll-anim')
+    .addTo(controller);
+
+var scrolldetail2 = new ScrollMagic.Scene({
+    triggerElement: '.potSize p',
+    triggerHook: 1,
+    reverse: true
+        })
+    .setClassToggle('.potSize p', 'scroll-anim')
     .addTo(controller);
