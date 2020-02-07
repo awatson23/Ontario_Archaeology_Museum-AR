c0f55535917f6245bc4c9a2c3f7c9005c41d2669
Ckmena <mena.kthy@gmail.com>
2020-02-07 13:01:01 -0500

    image transition

--- js/scrollmagic.js
+++ js/scrollmagic.js
@@ -7,3 +7,14 @@ var scrolldetail1 = new ScrollMagic.Scene({
      })
     .setClassToggle('.circ', 'scroll-anim')
     .addTo(controller);
+
+
+// slide in image animation
+
+    var scrollimage1 = new ScrollMagic.Scene({
+        triggerElement: '.img_1',
+        triggerHook: .8,
+        reverse:true
+         })
+        .setClassToggle('.img_1', 'scroll-img1')
+        .addTo(controller);
