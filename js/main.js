e5e60943ad5b6471420cb65cc2e31c9208bfd5b1
Marco DeLuca <mdeluca@swiftpixel.com>
2020-10-19 13:24:00 -0400

    Added new scanner code
    The old scanner was glitchy so it has been replaced with another library. The index and js files have beem updated.

--- js/main.js
+++ js/main.js
@@ -1,58 +1,58 @@
+  let resetButton = document.querySelector("#restartCamera")
 
-var video = document.createElement("video");
-var canvasElement = document.getElementById("canvas");
-var canvas = canvasElement.getContext("2d");
-var loadingMessage = document.getElementById("loadingMessage");
-var outputContainer = document.getElementById("output");
-var outputMessage = document.getElementById("outputMessage");
-var outputData = document.getElementById("outputData");
-
-function drawLine(begin, end, color) {
-  canvas.beginPath();
-  canvas.moveTo(begin.x, begin.y);
-  canvas.lineTo(end.x, end.y);
-  canvas.lineWidth = 4;
-  canvas.strokeStyle = color;
-  canvas.stroke();
-}
-
-// Use facingMode: environment to attemt to get the front camera on phones
-navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
-  video.srcObject = stream;
-  video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-  video.play();
-  requestAnimationFrame(tick);
-});
-
-function tick() {
-  loadingMessage.innerText = "⌛ Loading video..."
-  if (video.readyState === video.HAVE_ENOUGH_DATA) {
-    loadingMessage.hidden = true;
-    canvasElement.hidden = false;
-    outputContainer.hidden = false;
-
-    canvasElement.height = video.videoHeight;
-    canvasElement.width = video.videoWidth;
-    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-    var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-    var code = jsQR(imageData.data, imageData.width, imageData.height, {
-      inversionAttempts: "dontInvert",
-    });
-    if (code) {
-      drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
-      drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
-      drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
-      drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
-      outputMessage.hidden = true;
-      outputData.parentElement.hidden = false;
-      outputData.innerText = code.data;
-      window.location.assign(code.data);
-    } else {
-      outputMessage.hidden = false;
-      outputData.parentElement.hidden = true;
+  function onQRCodeScanned(scannedText)
+    {
+      var scannedTextMemo = document.querySelector("#scannedTextMemo");
+      if(scannedTextMemo)
+      {
+        scannedTextMemo.innerHTML = `Please Wait`;
+        window.location.assign(scannedText);
+      }
     }
-  }
-  requestAnimationFrame(tick);
-}
+    
+    //function returning a promise with a video stream
+  function provideVideoQQ()
+    {
+        return navigator.mediaDevices.enumerateDevices()
+        .then(function(devices) {
+            var exCameras = [];
+            devices.forEach(function(device) {
+            if (device.kind === 'videoinput') {
+              exCameras.push(device.deviceId)
+            }
+         });
+            
+            return Promise.resolve(exCameras);
+        }).then(function(ids){
+            if(ids.length === 0)
+            {
+              document.querySelector("#scannedTextMemo").innerHTML = "Your device does not have a camera";
+            } else {
+              var constraints= { video: { facingMode: "environment" } };
+              return navigator.mediaDevices.getUserMedia(constraints);       
+            }      
+        });               
+    }  
+  
+    //this function will be called when JsQRScanner is ready to use
+    function JsQRScannerReady()
+    {
+        //create a new scanner passing to it a callback function that will be invoked when
+        //the scanner succesfully scan a QR code
+        var jbScanner = new JsQRScanner(onQRCodeScanned, provideVideoQQ);
+        //reduce the size of analyzed images to increase performance on mobile devices
+        jbScanner.setSnapImageMaxSize(300);
+        var scannerParentElement = document.getElementById("scanner");
+      if(scannerParentElement)
+        {
+            //append the jbScanner to an existing DOM element
+          jbScanner.appendTo(scannerParentElement);
+        }        
+    }
 
+    function restartCamera()
+    {
+      location.reload();
+    }
 
+    resetButton.addEventListener("click", restartCamera);
