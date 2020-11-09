8b60d27ce0b35ae8359956037a484538928c959f
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-11-09 15:42:57 -0500

    Testing PWA caching

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "C:\\Users\\Noha\\Documents\\L5 vids\\5008\\pwa test\\test_files_k_n\\Ontario_Archaeology_Museum-AR\\",
+  "globPatterns": [
+    "**/*.{yml,html,js,json,ico,png,svg,txt,md,vue,css,jpg,usdz,JPG,xml,gif,mp4}"
+  ],
+  "swDest": "C:/Users/Noha/Documents/L5 vids/5008/pwa test/test_files_k_n/Ontario_Archaeology_Museum-AR/sw.js", 
+	"maximumFileSizeToCacheInBytes": 50000000
+};
